43a38a3599abf69eba0f7d1e0156f1c7516c457c
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-06-15 10:34:33 +0300

    add eslint and prettier (npm run format)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 
