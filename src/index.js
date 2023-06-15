a2280373e9b0c5b7705d662904dd361654eb9b7a
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-06-15 09:45:17 +0300

    add the project common architecture

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
