023e19bd9567549c654fbac7a2852b20487dd7b0
DVorortnoy <dimchik505@gmail.com>
2023-06-17 13:23:14 +0300

    merge

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import {Columns} from '../components/Сolumns/Columns';
+
 
 export const BoardPage = () => {
   return(
      <>
       <div>BoardPage</div>
-      <Columns />
      </>
      );
 };
