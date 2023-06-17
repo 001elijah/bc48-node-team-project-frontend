e9cc7f7bcb2db8f119cd591f897f49d471328f4d
Ihor Smyk <ismyk256@gmail.com>
2023-06-17 15:30:35 +0300

    fix

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -12,8 +12,6 @@ export const UserInfo = () => {
         className={s.avatar}
         src="https://cdn.pixabay.com/photo/2023/05/20/16/48/dog-8006839_960_720.jpg"
         alt="avatar"
-        width="32px"
-        height="32px"
       />
     </div>
   );
