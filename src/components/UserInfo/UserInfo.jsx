afa3b3f530665bbcde23ca794be1e66f2a4a985d
Ihor Smyk <ismyk256@gmail.com>
2023-06-19 18:52:18 +0300

    upd userInfo

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,15 +4,20 @@ import s from './UserInfo.module.scss';
 import icons from '../../assets/icons/sprite.svg';
 
 export const UserInfo = () => {
-  const userName = useSelector(state => state.auth?.user?.userName) || 'user';
-  const userAvatar =
-    useSelector(state => state.auth?.user?.avatarUrl) ||
-    'https://cdn.pixabay.com/photo/2023/05/20/16/48/dog-8006839_960_720.jpg';
+  const theme = useSelector(state => state.auth?.user?.theme);
+  const userName = useSelector(state => state.auth?.user?.userName);
+  const userAvatar = useSelector(state => state.auth?.user?.avatarUrl);
 
   return (
     <div className={s.userInfo}>
-      <div className={s.name}>{userName}</div>
-      <img className={s.avatar} src={userAvatar} alt="avatar" />
+      <div className={s.name}>{userName || 'user'}</div>
+      {userAvatar ? (
+        <img className={s.avatar} src={userAvatar} alt="avatar" />
+      ) : (
+        <svg className={s.avatar}>
+          <use href={`${icons}#icon-user-${theme || 'light'}`}></use>
+        </svg>
+      )}
     </div>
   );
 };
