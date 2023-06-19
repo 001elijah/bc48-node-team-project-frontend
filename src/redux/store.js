3ebe404a96d679e290e38e465d019407a2c260a1
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-06-19 10:35:33 +0300

    fix store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -31,7 +31,7 @@ export const store = configureStore({
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
-  devTools: process.env.NODE_ENV !== 'production',
+  // devTools: process.env.NODE_ENV !== 'production',
 });
 
 export const persistor = persistStore(store);
