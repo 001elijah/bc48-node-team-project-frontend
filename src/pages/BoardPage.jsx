4f80728c61adc91dc294e78ddcedbd7879e5113e
DVorortnoy <dimchik505@gmail.com>
2023-06-19 21:15:52 +0300

    add

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import {Columns} from '../components/Сolumns/Columns';
 
 export const BoardPage = () => {
   return(
      <>
       <div>BoardPage</div>
-      <Columns />
      </>
      );
 };
