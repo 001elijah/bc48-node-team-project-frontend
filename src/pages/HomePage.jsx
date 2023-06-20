57191138876afcad66fcf72749c0731084a53eba
GGalina <71042425+GGalina@users.noreply.github.com>
2023-06-20 16:53:27 +0400

    added home page to include button for modal

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,25 @@ import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from 'redux/Auth/authOperations';
 import { themeChangeUser } from 'redux/Auth/authOperations';
 import { selectorTheme } from 'redux/Auth/authSelectors';
+import React, { useState } from 'react';
+import { Modal } from '../components/Modal/Modal';
 
 export const HomePage = () => {
   const theme = useSelector(selectorTheme) || '';
   const dispatch = useDispatch();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleChange = e => {
     dispatch(themeChangeUser(e.target.value));
+  };
+
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -19,35 +31,14 @@ export const HomePage = () => {
         <option value="colorful">Colorful</option>
       </select>
       <button onClick={() => dispatch(logoutUser())}>Log logOut</button>
+
+      <button onClick={openModal}>Open Modal</button>
+
+      {isModalOpen && (
+       <Modal title="New board" onClose={closeModal}>
+         <p>Content of the modal.</p>
+       </Modal>
+       )}
     </div>
   );
 };
-
-// HomePage to test modal window
-// import React, { useState } from 'react';
-// import { Modal } from '../components/Modal/Modal';
-
-// export const HomePage = () => {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   const openModal = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   return (
-//     <div>
-//       <h1>Welcome to My Website</h1>
-//       <button onClick={openModal}>Open Modal</button>
-
-//       {isModalOpen && (
-//         <Modal title="New board" onClose={closeModal}>
-//           <p>Content of the modal.</p>
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// };
