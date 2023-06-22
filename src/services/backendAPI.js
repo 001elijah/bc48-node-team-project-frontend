8e247943b6f1d03e8338285c3085c351eb895fd6
Vladyslav-H <97888612+Vladyslav-H@users.noreply.github.com>
2023-06-22 22:40:55 +0300

    create column operation remove

--- src/services/backendAPI.js
+++ src/services/backendAPI.js
@@ -40,9 +40,12 @@ export const themeChangeUserApi = async theme => {
   return data;
 };
 
-// export const removeColumnApi = async id => {
-//   await axios.delete(`/column/${id}`);
-// };
-// export const removeCardApi = async id => {
-//   await axios.delete(`/card/${id}`);
-// };
+export const removeColumnApi = async columnId => {
+  const id = await axios.delete(`/column/${columnId}`);
+  return id;
+};
+
+export const removeCardApi = async cardId => {
+  const id = await axios.delete(`/card/${cardId}`);
+  return id;
+};
