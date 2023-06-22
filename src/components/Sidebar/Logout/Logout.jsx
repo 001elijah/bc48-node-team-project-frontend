431c123c1760bccbf0bb81021fb738c8b6887da3
Berlinchik <davidarts11@gmail.com>
2023-06-22 18:42:10 +0300

    update logout button

--- src/components/Sidebar/Logout/Logout.jsx
+++ src/components/Sidebar/Logout/Logout.jsx
@@ -2,11 +2,19 @@ import s from './Logout.module.scss';
 import sprite from '../../../assets/icons/sprite.svg';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { logoutUser } from '../../../redux/Auth/authOperations';
 
 const Logout = ({ theme }) => {
+  const dispatch = useDispatch();
+
   return (
     <div className={s.buttonWrapper}>
-      <button type="button" className={clsx(s.button, s[theme])}>
+      <button
+        type="button"
+        className={clsx(s.button, s[theme])}
+        onClick={() => dispatch(logoutUser())}
+      >
         <svg className={clsx(s.icon, s[theme])}>
           <use href={`${sprite}#icon-logout`}></use>
         </svg>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { logoutUser } from 'redux/Auth/authOperations';
 import Sidebar from '../components/Sidebar/Sidebar';
 
 export const HomePage = () => {
-  const dispatch = useDispatch();
   return (
     <div style={{ display: 'flex' }}>
       <Sidebar />
-
-      <button onClick={() => dispatch(logoutUser())}>Log logOut</button>
     </div>
   );
 };
