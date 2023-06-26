290a495c8c6e3897ea69cedcf6f54200aec127ff
GGalina <71042425+GGalina@users.noreply.github.com>
2023-06-26 16:45:00 +0400

    fix

--- src/services/backendAPI.js
+++ src/services/backendAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://taskpro.onrender.com/user';
+axios.defaults.baseURL = 'https://taskpro.onrender.com';
 
 //axios.defaults.baseURL = 'http://localhost:3000';
 
