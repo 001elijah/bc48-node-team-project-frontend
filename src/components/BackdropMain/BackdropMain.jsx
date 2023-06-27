c291b90dc2aca0f17fdd059846c73fe90eb1e494
GGalina <71042425+GGalina@users.noreply.github.com>
2023-06-27 16:53:24 +0400

    fixed help window.

--- src/components/BackdropMain/BackdropMain.jsx
+++ src/components/BackdropMain/BackdropMain.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
 import s from './BackdropMain.module.scss';
@@ -6,27 +6,73 @@ import s from './BackdropMain.module.scss';
 const modalRoot = document.querySelector('#modal-root');
 
 export const BackdropModal = ({ children, closeModal }) => {
-  const handleClose = useCallback(
-    event => {
-      if (event.target === event.currentTarget || event.code === 'Escape') {
-        closeModal();
-      }
-    },
-    [closeModal],
-  );
+  const backdropRef = useRef(null);
+  const modalRef = useRef(null);
+  const isMouseDownRef = useRef(false);
+
+  const handleClose = useCallback(() => {
+    closeModal();
+  }, [closeModal]);
+
+  const handleMouseDown = useCallback(() => {
+    isMouseDownRef.current = true;
+  }, []);
+
+  const handleMouseUp = useCallback(() => {
+    if (isMouseDownRef.current) {
+      isMouseDownRef.current = false;
+    } else {
+      handleClose();
+    }
+  }, [handleClose]);
+
+  const handleModalClick = useCallback(event => {
+    event.stopPropagation();
+  }, []);
 
   useEffect(() => {
-    window.addEventListener('keydown', handleClose);
+    const handleKeyDown = event => {
+      if (event.key === 'Escape') {
+        event.stopPropagation();
+        handleClose();
+      }
+    };
 
+    window.addEventListener('keydown', handleKeyDown);
     return () => {
-      window.removeEventListener('keydown', handleClose);
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleClose]);
+
+  useEffect(() => {
+    const handleMouseUpOutsideModal = () => {
+      if (isMouseDownRef.current) {
+        isMouseDownRef.current = false;
+        handleClose();
+      }
+    };
+
+    document.addEventListener('mouseup', handleMouseUpOutsideModal);
+    return () => {
+      document.removeEventListener('mouseup', handleMouseUpOutsideModal);
     };
   }, [handleClose]);
 
   return createPortal(
-    <div className={s.backdrop} onClick={handleClose}>
-      {children}
+    <div
+      className={s.backdrop}
+      onMouseUp={handleMouseUp}
+      ref={backdropRef}
+    >
+      <div
+        className={s.modal}
+        onClick={handleModalClick}
+        onMouseDown={handleMouseDown}
+        ref={modalRef}
+      >
+        {children}
+      </div>
     </div>,
-    modalRoot,
+    modalRoot
   );
 };
