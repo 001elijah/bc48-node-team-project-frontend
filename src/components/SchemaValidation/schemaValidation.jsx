dd39ef6db81cfed0ed18ed209565c1c6e0cde42e
Andrii <ifromukraine2022@gmail.com>
2023-06-29 21:07:45 +0300

    add valid name

--- src/components/SchemaValidation/schemaValidation.jsx
+++ src/components/SchemaValidation/schemaValidation.jsx
@@ -23,12 +23,8 @@ export const validationSchemaLogin = yup.object().shape({
 export const validationSchemaRegister = yup.object().shape({
   userName: yup
     .string()
-    // .matches(
-    //   /^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)?$/,
-    //   'Only letters, numbers, underscores, and spaces are allowed',
-    // )
     .matches(
-    /^[a-zA-Z0-9]+([ -]?[a-zA-Z0-9])*$/,
+    /^[a-zA-Z0-9]+([-_ ][a-zA-Z0-9]+)*$/,
     'Only letters, numbers, underscores, spaces, hyphens, and dashes are allowed between words',
   )
     .min(2, 'Minimum length 2 characters')
@@ -51,38 +47,4 @@ export const validationSchemaRegister = yup.object().shape({
     .min(8, 'Minimum length 8 characters')
     .max(64, 'Maximum length 64 characters')
     .required('Password required'),
-});
-
-export const validationSchemaEditProfile = yup.object().shape({
-  userName: yup
-    .string()
-    // .matches(
-    //   /^[a-zA-Z0-9]+([ -]?[a-zA-Z0-9])*$/,
-    //   'Only letters, numbers, underscores, and spaces are allowed',
-    // )
-    .matches(
-    /^[a-zA-Z0-9]+([ -]?[a-zA-Z0-9])*$/,
-    'Only letters, numbers, underscores, spaces, hyphens, and dashes are allowed between words',
-  )
-    .min(2, 'Minimum length 2 characters')
-    .max(32, 'Maximum length 32 characters')
-    .required('Name required'),
-  email: yup
-    .string()
-    .matches(
-      /^[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      'Invalid email address. Example: a@a.ua',
-    )
-    .required('Email required'),
-  password: yup
-    .string()
-    .matches(/^\S*$/, 'Must not contain spaces')
-    .matches(
-      /^[a-zA-Z0-9!"@#$%^&*()]+$/,
-      'Can only contain Latin letters, numbers, and signs',
-    )
-    .min(8, 'Minimum length 8 characters')
-    .max(64, 'Maximum length 64 characters')
-    .required('Password required'),
-  avatarUrl: yup.string(),
 });
