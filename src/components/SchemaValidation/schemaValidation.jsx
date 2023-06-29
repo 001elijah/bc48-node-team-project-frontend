d27e62012adcec06e206c0c8a0fc1d947065e19e
Andrii <ifromukraine2022@gmail.com>
2023-06-29 22:16:25 +0300

    valid

--- src/components/SchemaValidation/schemaValidation.jsx
+++ src/components/SchemaValidation/schemaValidation.jsx
@@ -25,7 +25,8 @@ export const validationSchemaRegister = yup.object().shape({
     .string()
     .matches(
     /^[a-zA-Z0-9]+([-_ ][a-zA-Z0-9]+)*$/,
-    'Only letters, numbers, underscores, spaces, hyphens, and dashes are allowed between words',
+    // 'Only letters, numbers, underscores, spaces, hyphens, and dashes are allowed between words',
+      'Invalid name'
   )
     .min(2, 'Minimum length 2 characters')
     .max(32, 'Maximum length 32 characters')
